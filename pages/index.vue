81ae3531e5269e270bc5fd1bebcd466084e88e1a
Egor Melnichenko <e.melnichenko@erdc.ru>
2022-08-20 23:26:42 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>hi, i'm index</h1>
+    <nuxt-link to="about">about</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
